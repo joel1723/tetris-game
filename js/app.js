6388a835681706b69c0ff6b6ed9186e936c47691
joel1723 <joeljean1723@gmail.com>
2021-06-23 12:18:40 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMcontentLoaded',() =>{
+    const grid = document.querySelector('.grid');
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const width = 10
+    console.log(squares)
+})
